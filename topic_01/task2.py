925059a1146e48a212326f7d3a73fe74f003be0f
Shatun Ivan <fox4097fox@gmail.com>
2025-12-17 22:37:37 +0200

    Оновлення файлу 2 топіку

--- topic_01/task2.py
+++ topic_01/task2.py
@@ -1,7 +1,9 @@
-text = input("Введіть рядок: ")
+def print_str_methods(text):
+    print("strip():", text.strip())
+    print("capitalize():", text.capitalize())
+    print("title():", text.title())
+    print("upper():", text.upper())
+    print("lower():", text.lower())
 
-print("strip():", text.strip())
-print("capitalize():", text.capitalize())
-print("title():", text.title())
-print("upper():", text.upper())
-print("lower():", text.lower())
+s = input("Введіть рядок: ")
+print_str_methods(s)
